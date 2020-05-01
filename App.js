fa24fa19df04c12c195a4f607ed2ad28e6774320
kharioki <antolisah@gmail.com>
2020-05-01 21:46:57 +0300

    rendering stars

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import {StyleSheet, View, Text, StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Animated,
+} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+FontAwesome.loadFont();
+
+const numStars = 5;
 
 const App = () => {
+  const [rating, setRating] = useState();
+
+  let stars = [];
+
+  for (let i = 1; i <= numStars; i++) {
+    stars.push(
+      <TouchableWithoutFeedback key={i}>
+        <Animated.View>
+          <Star />
+        </Animated.View>
+      </TouchableWithoutFeedback>,
+    );
+  }
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.container}>
-        <Text>Screen</Text>
-      </View>
-    </>
+    <View style={styles.container}>
+      <View style={{flexDirection: 'row'}}>{stars}</View>
+    </View>
+  );
+};
+
+const Star = () => {
+  return (
+    <FontAwesome
+      name="star"
+      color="purple"
+      size={32}
+      style={{marginHorizontal: 6}}
+    />
   );
 };
 
